26575067f50deb3ede72e8ef0bcce22feaa8aa19
TurBoss <j.l.toledano.l@gmail.com>
2020-02-18 03:56:31 +0100

    take main args

--- create_config.py
+++ create_config.py
@@ -1,26 +1,36 @@
+import sys
 import git
 from xml.etree import ElementTree
 
-in_config_file = "config/config.xml"
-out_config_file = "config/build_config.xml"
 
-tree = ElementTree.parse(in_config_file)
-root = tree.getroot()
+def main(args):
 
-repo = git.Repo("..")
-sha = repo.head.object.hexsha
+    print(args)
 
-# version_tag = next((tag for tag in repo.tags if tag.commit == repo.head.commit), None)
-version_tag = None
-for tag in repo.tags:
-    version_tag = tag
+    in_config_file = args[1]
+    out_config_file = args[2]
 
-if version_tag:
-    version = version_tag
-    print(version)
+    tree = ElementTree.parse(in_config_file)
+    root = tree.getroot()
 
-    version_xml = root.find(".//Version")
-    release_version = "{}_git{}".format(version, sha[:6])
-    print(release_version)
-    version_xml.text = release_version
-    tree.write(out_config_file, encoding='utf-8', xml_declaration=True)
+    repo = git.Repo("..")
+    sha = repo.head.object.hexsha
+
+    # version_tag = next((tag for tag in repo.tags if tag.commit == repo.head.commit), None)
+    version_tag = None
+    for tag in repo.tags:
+        version_tag = tag
+
+    if version_tag:
+        version = version_tag
+        print(version)
+
+        version_xml = root.find(".//Version")
+        release_version = "{}_git{}".format(version, sha[:6])
+        print(release_version)
+        version_xml.text = release_version
+        tree.write(out_config_file, encoding='utf-8', xml_declaration=True)
+
+
+if __name__ == "__main__":
+    main(sys.argv)
