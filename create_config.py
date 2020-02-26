0578a785e4f1b1101b2d6ad21637b5acdec5d01e
TurBoss <j.l.toledano.l@gmail.com>
2020-02-26 16:22:59 +0100

    select repo url from build

--- create_config.py
+++ create_config.py
@@ -6,8 +6,13 @@ from qtpyvcp._version import get_versions
 
 def main(args):
 
+    main_url = "http://repository.qtpyvcp.com/main/repo/"
+    dev_url = "http://repository.qtpyvcp.com/dev/repo/"
+
     in_config_file = args[1]
     out_config_file = args[2]
+
+    url = args[3]
 
     tree = ElementTree.parse(in_config_file)
     root = tree.getroot()
@@ -16,6 +21,10 @@ def main(args):
     print(release_version)
     version_xml = root.find(".//Version")
     version_xml.text = release_version
+
+    url_xml = root.find(".//Url")
+    url_xml.text = url
+
     tree.write(out_config_file, encoding='utf-8', xml_declaration=True)
 
 
