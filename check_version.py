4ff5f53e9fc46ba8892b06f4c0aaf30c69591a5e
TurBoss <j.l.toledano.l@gmail.com>
2020-06-02 15:02:41 +0200

    get the version from stdout

--- check_version.py
+++ check_version.py
@@ -4,7 +4,7 @@ from qtpyvcp._version import get_versions
 def main():
 
     release_version = get_versions().get('version')[1:]
-    return release_version
+    print(release_version)
 
 
 if __name__ == "__main__":
