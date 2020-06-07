a3badce17e541a2d6dd0153948c31c6b23194389
TurBoss <j.l.toledano.l@gmail.com>
2020-06-07 13:39:22 +0200

    config and package config scripts

--- create_package_config.py
+++ create_package_config.py
@@ -6,19 +6,13 @@ def main(args):
 
     in_config_file = args[1]
     out_config_file = args[2]
-
-    url = args[3]
-
-    release_version = args[4]
+    release_version = args[3]
 
     tree = ElementTree.parse(in_config_file)
     root = tree.getroot()
 
     version_xml = root.find(".//Version")
     version_xml.text = release_version
-
-    url_xml = root.find(".//Url")
-    url_xml.text = url
 
     tree.write(out_config_file, encoding='utf-8', xml_declaration=True)
 
